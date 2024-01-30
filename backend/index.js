5203276df435b94e2b30d999ffd585b67e94ee19
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-01-30 23:32:38 +0530

    (complete) Backend

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@
 const express = require("express");
 const { createTodo, updateTodo } = require("./types");
 const app = express();
+const { todo } = require('./db.js');
 
 // body {
 // title: string;
@@ -15,7 +16,7 @@ const app = express();
 app.use(express.json()); // middleware
 
 // POST endpoint for adding a new TODO 
-app.post("/todo", function(req, res){
+app.post("/todo",async function(req, res){
     const createPayload = req.body;
     // The zod object allows us to parse an incoming JSON using the safeParse mtd
     const parsedPayload = createTodo.safeParse(createPayload);
@@ -25,16 +26,33 @@ app.post("/todo", function(req, res){
         });
         return;
     }
+
     // put it in MongoDB
+    // Using await syntax here because if the database is down
+    // it would throw an exception and we need to setup global 
+    // catch to handle that.
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed: false,
+    });
+
+    res.json({
+        msg: "Todo created",
+    })
 });
 
 // GET endpoint for getting all the TODOs
-app.get("/todo", function(req, res){
+app.get("/todo", async function(req, res){
+    const todos = await todo.find({});
 
+    res.json({
+        todos,
+    })
 });
 
-// 
-app.put("/completed", function(req, res){
+// PUT endpoint for updating an existing TODO
+app.put("/completed", async function(req, res){
     const updatePayload = req.body;
     const parsedPayload = updateTodo.safeParse(updatePayload);
     if(!parsedPayload.success){
@@ -43,5 +61,13 @@ app.put("/completed", function(req, res){
         });
         return;
     }
-    // put it in MongoDB
+    // put it in MongoDB -- need clarity
+    await todo.update({
+        _id: req.body.id,
+    }, {
+            completed: true,
+        });
+    res.json({
+        msg: "Todo marked as complete",
+    });
 });

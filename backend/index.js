6fdb4e97c3fc0ea6435a255299cef0c81cfe5f6f
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-04-15 14:03:40 +0530

    Starting work on backend

--- backend/index.js
+++ backend/index.js
@@ -1,75 +1,38 @@
-// write basic express boilerplate code,
-// with express.json() middleware
-// NOT authenticated application
-
 const express = require("express");
-const { createTodo, updateTodo } = require("./types");
+const { createTodo, updateTodo } = require("./types/types");
+
 const app = express();
-const { todo } = require('./db.js');
+
+app.use(express.json());
 
 // body {
-// title: string;
-// description: string;
+//    title : 
+//    description : 
 // }
-
-app.use(express.json()); // middleware
-
-// POST endpoint for adding a new TODO 
-app.post("/todo", async function(req, res){
+app.post("/todo", (req, res) => {
     const createPayload = req.body;
-    // The zod object allows us to parse an incoming JSON using the safeParse mtd
-    const parsedPayload = createTodo.safeParse(createPayload);
-    if(!parsedPayload.success){ 
+    const parsedPayload = createTodo.safeParse(createPayload)
+    if (!parsedPayload.success) {
         res.status(411).json({
-            msg: "You sent the wrong inputs",
+            msg: "You sent the wrong inputs"
         });
         return;
     }
-
-    // put it in MongoDB
-    // Using await syntax here because if the database is down
-    // it would throw an exception and we need to setup global 
-    // catch to handle that.
-    await todo.create({
-        title: createPayload.title,
-        description: createPayload.description,
-        completed: false,
-    });
-
-    res.json({
-        msg: "Todo created",
-    })
-});
-
-// GET endpoint for getting all the TODOs
-app.get("/todo", async function(req, res){
-    const todos = await todo.find({});
-
-    res.json({
-        todos,
-    })
-});
-
-// PUT endpoint for updating an existing TODO
-app.put("/completed", async function(req, res){
+    // put it inside MongoDB 
+})
+app.get("/todos", (req, res) => {
+    // Fetch all TODOs from MongoDB
+})
+app.put("/completed", (req, res) => {
     const updatePayload = req.body;
-    const parsedPayload = updateTodo.safeParse(updatePayload);
-    if(!parsedPayload.success){
+    const parsedPayload = updateTodo.safeParse(updatePayload)
+    if (!parsedPayload.success) {
         res.status(411).json({
-            msg: "You send the wrong inputs",
-        });
+            msg: "You sent the wrong inputs"
+        })
         return;
     }
+    // update it inside MongoDB
+})
 
-    // put it in MongoDB -- need clarity
-    await todo.update({
-        _id: req.body.id,
-    }, {
-            completed: true,
-        });
-    res.json({
-        msg: "Todo marked as complete",
-    });
-});
-
-app.listen(3000);
+app.listen(3000)

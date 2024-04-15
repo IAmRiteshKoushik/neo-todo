8ea815d2c0163ad008236dbb78e115048c5301e1
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-04-15 15:46:19 +0530

    Add front-end code

---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,9 @@
+export const CreateTodo = () => {
+    return(
+        <div>
+            <input type="text" placeholder="title"></input>
+            <input type="text" placeholder="description"></input>
+            <button>Add a Todo</button>
+        </div>
+    );
+}
